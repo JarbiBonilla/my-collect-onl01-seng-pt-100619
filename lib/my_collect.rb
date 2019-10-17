47bdf0b8f897ef0586d5f64cdbf2f7f680260c0d
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-17 05:00:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   new_collections = []
   while i < collection.length
-  new_collections << yield(collection[1])
+  new_collections << yield(collection[i])
   i += 1
  end
 new_collections
